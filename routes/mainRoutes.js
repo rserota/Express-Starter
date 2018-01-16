b817d85aea5a0869ea08d916005d8bf3d316f2ff
raphael.serota <raphael.serota@gmail.com>
2018-01-15 19:16:36 -0500

    updated bootstrap

--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -3,7 +3,15 @@ var router = express.Router()
 
 
 router.get('/', function(req, res) {
-    res.render('index')
+    res.redirect('/about')
+})
+
+router.get('/about', function(req, res) {
+    res.render('about')
+})
+
+router.get('/portfolio', function(req, res) {
+    res.render('portfolio')
 })
 
 module.exports = router
